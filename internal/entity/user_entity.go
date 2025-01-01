ab05085da6aae556b6c61b8a82f211690dab81a4
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-01 21:39:32 +0700

    add AccessToken field to User entity for enhanced authentication

--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -1,14 +1,15 @@
 package entity
 
 type User struct {
-	ID        string `gorm:"column:id;primaryKey;"`
-	Name      string `gorm:"column:name;"`
-	Email     string `gorm:"column:email;"`
-	Password  string `gorm:"column:password;"`
-	Token     string `gorm:"column:token;"`
-	CreatedAt int64  `gorm:"column:created_at;autoCreateTime:milli;"`
-	UpdatedAt int64  `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli;"`
-	Task      []Task `gorm:"foreignKey:user_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID          string `gorm:"column:id;primaryKey;"`
+	Name        string `gorm:"column:name;"`
+	Email       string `gorm:"column:email;"`
+	Password    string `gorm:"column:password;"`
+	Token       string `gorm:"column:token;"`
+	AccessToken string `gorm:"-"`
+	CreatedAt   int64  `gorm:"column:created_at;autoCreateTime:milli;"`
+	UpdatedAt   int64  `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli;"`
+	Task        []Task `gorm:"foreignKey:user_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (User) TableName() string {
