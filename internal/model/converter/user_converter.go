0ca8cb4d8b881c22d6444fa0f39c10519509511d
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-01 17:42:09 +0700

    add user converter for transforming user entity to response models

---
+++ internal/model/converter/user_converter.go
@@ -0,0 +1,23 @@
+package converter
+
+import (
+	"github.com/abdisetiakawan/go-clean-arch/internal/model"
+
+	"github.com/abdisetiakawan/go-clean-arch/internal/entity"
+)
+
+func UserToResponse(user *entity.User) *model.UserResponse {
+	return &model.UserResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
+
+func UserToTokenResponse(user *entity.User) *model.UserResponse {
+	return &model.UserResponse{
+		Token: user.Token,
+	}
+}
