ea8dbac3dcc391fe9ef0c1a5df1c22f9e95a0491
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-01 21:40:08 +0700

    add token generation to user creation and login processes

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -4,77 +4,92 @@ import (
 	"context"
 
 	"github.com/abdisetiakawan/go-clean-arch/internal/entity"
+	"github.com/abdisetiakawan/go-clean-arch/internal/helper"
 	"github.com/abdisetiakawan/go-clean-arch/internal/model"
 	"github.com/abdisetiakawan/go-clean-arch/internal/model/converter"
 	"github.com/abdisetiakawan/go-clean-arch/internal/repository"
 	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber"
-	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type UserUseCase struct {
-	DB             *gorm.DB
-	Log            *logrus.Logger
-	Validate       *validator.Validate
-	UserRepository *repository.UserRepository
+    DB             *gorm.DB
+    Log            *logrus.Logger
+    Validate       *validator.Validate
+    UserRepository *repository.UserRepository
+    Helper         helper.Helper
 }
 
-func NewUserUseCase(db *gorm.DB, log *logrus.Logger, validate *validator.Validate, userRepository *repository.UserRepository) *UserUseCase {
-	return &UserUseCase{
-		DB:             db,
-		Log:            log,
-		Validate:       validate,
-		UserRepository: userRepository,
-	}
+func NewUserUseCase(db *gorm.DB, log *logrus.Logger, validate *validator.Validate, userRepository *repository.UserRepository, helper helper.Helper) *UserUseCase {
+    return &UserUseCase{
+        DB:             db,
+        Log:            log,
+        Validate:       validate,
+        UserRepository: userRepository,
+        Helper:         helper,
+    }
 }
 
 func (c *UserUseCase) Create(ctx context.Context, request *model.CreateUserRequest) (*model.UserResponse, error) {
-	tx := c.DB.WithContext(ctx).Begin()
-	defer tx.Rollback()
+    tx := c.DB.WithContext(ctx).Begin()
+    defer tx.Rollback()
 
-	err := c.Validate.Struct(request)
-	if err != nil {
-		c.Log.Warnf("Failed to validate request body : %+v", err)
-		return nil, fiber.ErrBadRequest
-	}
-	total, err := c.UserRepository.CountById(tx, request.ID)
-	if err != nil {
-		c.Log.Warnf("Failed to count user : %+v", err)
-		return nil, fiber.ErrInternalServerError
-	}
-	if total > 0 {
-		c.Log.Warnf("User already exists : %+v", err)
-		return nil, fiber.ErrConflict
-	}
-	password, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
-	if err != nil {
-		c.Log.Warnf("Failed to hash password : %+v", err)
-		return nil, fiber.ErrInternalServerError
-	}
+    err := c.Validate.Struct(request)
+    if err != nil {
+        c.Log.Warnf("Failed to validate request body : %+v", err)
+        return nil, fiber.ErrBadRequest
+    }
+    
+    total, err := c.UserRepository.CountById(tx, request.ID)
+    if err != nil {
+        c.Log.Warnf("Failed to count user : %+v", err)
+        return nil, fiber.ErrInternalServerError
+    }
+    if total > 0 {
+        c.Log.Warnf("User already exists : %+v", err)
+        return nil, fiber.ErrConflict
+    }
+    
+    password, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+    if err != nil {
+        c.Log.Warnf("Failed to hash password : %+v", err)
+        return nil, fiber.ErrInternalServerError
+    }
 
-	user := &entity.User{
-		ID:       request.ID,
-		Name:     request.Name,
-		Email:    request.Email,
-		Password: string(password),
-	}
+    accessToken, refreshToken, err := c.Helper.GenerateTokenUser(model.UserResponse{
+        Name:  request.Name,
+        Email: request.Email,
+    })
+    if err != nil {
+        c.Log.Warnf("Failed to generate tokens : %+v", err)
+        return nil, fiber.ErrInternalServerError
+    }
 
-	err = c.UserRepository.Create(tx, user)
-	if err != nil {
-		c.Log.Warnf("Failed to create user : %+v", err)
-		return nil, fiber.ErrInternalServerError
-	}
+    user := &entity.User{
+        ID:           request.ID,
+        Name:         request.Name,
+        Email:        request.Email,
+        Password:     string(password),
+        AccessToken:  accessToken,
+        Token: refreshToken,
+    }
 
-	err = tx.Commit().Error
-	if err != nil {
-		c.Log.Warnf("Failed to commit transaction : %+v", err)
-		return nil, fiber.ErrInternalServerError
-	}
+    err = c.UserRepository.Create(tx, user)
+    if err != nil {
+        c.Log.Warnf("Failed to create user : %+v", err)
+        return nil, fiber.ErrInternalServerError
+    }
 
-	return converter.UserToResponse(user), nil
+    err = tx.Commit().Error
+    if err != nil {
+        c.Log.Warnf("Failed to commit transaction : %+v", err)
+        return nil, fiber.ErrInternalServerError
+    }
+
+    return converter.UserToResponse(user), nil
 }
 
 func (c *UserUseCase) Login(ctx context.Context, request *model.LoginUserRequest) (*model.UserResponse, error) {
@@ -86,7 +101,9 @@ func (c *UserUseCase) Login(ctx context.Context, request *model.LoginUserRequest
 		c.Log.Warnf("Failed to validate request body : %+v", err)
 		return nil, fiber.ErrBadRequest
 	}
+
 	user := new(entity.User)
+
 	err = c.UserRepository.FindByEmail(tx, user, request.Email)
 	if err != nil {
 		c.Log.Warnf("Failed to find user : %+v", err)
@@ -98,7 +115,18 @@ func (c *UserUseCase) Login(ctx context.Context, request *model.LoginUserRequest
 		c.Log.Warnf("Failed to compare password : %+v", err)
 		return nil, fiber.ErrUnauthorized
 	}
-	user.Token = uuid.New().String()
+
+	accessToken, refreshToken, err := c.Helper.GenerateTokenUser(model.UserResponse{
+		Name:  user.Name,
+		Email: user.Email,
+	})
+	if err != nil {
+		c.Log.Warnf("Failed to generate tokens : %+v", err)
+		return nil, fiber.ErrInternalServerError
+	}
+
+	user.AccessToken = accessToken
+	user.Token = refreshToken
 
 	err = c.UserRepository.Update(tx, user)
 	if err != nil {
