da6d77f0e0ed13adb6b2e44505b7b3e8fb9c7717
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 20:02:32 +0700

    refactor user repository to remove database dependency and eliminate unused FindByToken function

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"github.com/abdisetiakawan/go-clean-arch/internal/entity"
 	"github.com/sirupsen/logrus"
-	"gorm.io/gorm"
 )
 
 type UserRepository struct {
@@ -11,12 +10,8 @@ type UserRepository struct {
 	Log *logrus.Logger
 }
 
-func NewUserRepository(db *gorm.DB, log *logrus.Logger) *UserRepository {
+func NewUserRepository(log *logrus.Logger) *UserRepository {
 	return &UserRepository{
 		Log: log,
 	}
-}
-
-func (r *UserRepository) FindByToken (db *gorm.DB, user *entity.User, token string) error {
-	return db.Where("token = ?", token).First(user).Error
 }
