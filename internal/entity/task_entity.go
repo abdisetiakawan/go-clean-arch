2e56a09d87c3c8ecaa27f3444fa3159ab2317745
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 20:05:22 +0700

    refactor task entity to use email instead of user ID and update due date type

--- internal/entity/task_entity.go
+++ internal/entity/task_entity.go
@@ -4,11 +4,15 @@ import "time"
 
 type Task struct {
 	ID          uint      `gorm:"column:id;primaryKey;autoIncrement"`
-	UserID      uint      `gorm:"column:user_id;not null"`
+	Email      	string      `gorm:"column:email;not null"`
 	Title       string    `gorm:"column:title;size:150;not null"`
 	Description string    `gorm:"column:description;type:text"`
 	Status      string    `gorm:"column:status;type:enum('pending','in_progress','completed');default:'pending'"`
-	DueDate     *time.Time `gorm:"column:due_date;type:date"`
+	DueDate     time.Time `gorm:"column:due_date;type:date"`
 	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt   time.Time `gorm:"column:updated_at;autoUpdateTime"`
+}
+
+func (Task) TableName() string {
+	return "tasks"
 }
