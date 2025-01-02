896618635092e2ddbe4c6b9a3f9fe50ce9e0daf4
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 19:57:23 +0700

    enhance auth middleware to validate JWT tokens and extract user email from claims

--- internal/delivery/http/middleware/auth_middleware.go
+++ internal/delivery/http/middleware/auth_middleware.go
@@ -1,26 +1,41 @@
 package middleware
 
 import (
+	"strings"
+
 	"github.com/abdisetiakawan/go-clean-arch/internal/model"
 	"github.com/abdisetiakawan/go-clean-arch/internal/usecase"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"github.com/spf13/viper"
 )
 
-func NewAuth(userUserCase *usecase.UserUseCase) fiber.Handler {
-	return func(ctx *fiber.Ctx) error {
-		request := &model.VerifyUserRequest{Token: ctx.Get("Authorization", "NOT_FOUND")}
-		userUserCase.Log.Debugf("Authorization : %s", request.Token)
+func NewAuth(userUserCase *usecase.UserUseCase, viper *viper.Viper) fiber.Handler {
+    return func(ctx *fiber.Ctx) error {
+        authHeader := ctx.Get("Authorization")
+        if authHeader == "" {
+            return fiber.ErrUnauthorized
+        }
+		secretkey := viper.GetString("credentials.accesssecret")
+        tokenString := strings.TrimPrefix(authHeader, "Bearer ")	
+        token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+            return []byte(secretkey), nil
+        })
+        if err != nil || !token.Valid {
+            return fiber.ErrUnauthorized
+        }
 
-		auth, err := userUserCase.Verify(ctx.UserContext(), request)
-		if err != nil {
-			userUserCase.Log.Warnf("Failed find user by token : %+v", err)
-			return fiber.ErrUnauthorized
-		}
+        claims, ok := token.Claims.(jwt.MapClaims)
+        if !ok || !token.Valid {
+            return fiber.ErrUnauthorized
+        }
 
-		userUserCase.Log.Debugf("User : %+v", auth.Email)
-		ctx.Locals("auth", auth)
-		return ctx.Next()
-	}
+        auth := &model.Auth{
+            Email: claims["email"].(string),
+        }
+        ctx.Locals("auth", auth)
+        return ctx.Next()
+    }
 }
 
 func GetUser(ctx *fiber.Ctx) *model.Auth {
