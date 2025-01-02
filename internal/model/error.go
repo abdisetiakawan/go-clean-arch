fbad50ca782e08208672c22909421955d6d4450a
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 09:25:12 +0700

    add custom API error model for improved error handling

---
+++ internal/model/error.go
@@ -0,0 +1,26 @@
+package model
+
+import "github.com/gofiber/fiber"
+
+type ApiError struct {
+    StatusCode int    `json:"-"`
+    Message    string `json:"message"`
+}
+
+func (e *ApiError) Error() string {
+    return e.Message
+}
+
+func NewApiError(statusCode int, message string) *ApiError {
+    return &ApiError{
+        StatusCode: statusCode,
+        Message:    message,
+    }
+}
+
+var (
+    ErrUserAlreadyExists  = NewApiError(fiber.StatusConflict, "User already exists")
+    ErrInvalidCredentials = NewApiError(fiber.StatusUnauthorized, "Invalid credentials")
+    ErrBadRequest        = NewApiError(fiber.StatusBadRequest, "Invalid request")
+    ErrInternalServer    = NewApiError(fiber.StatusInternalServerError, "Internal server error")
+)
