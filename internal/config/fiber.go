e7ebe2eb15ca0bb2d2e3522b5b4e35f1cdad4032
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 09:24:03 +0700

    refactor error handling in Fiber configuration to support custom API errors

--- internal/config/fiber.go
+++ internal/config/fiber.go
@@ -1,29 +1,37 @@
+// internal/config/fiber.go
 package config
 
 import (
+	"github.com/abdisetiakawan/go-clean-arch/internal/model"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
 )
 
 func NewFiber(config *viper.Viper) *fiber.App {
-	var app = fiber.New(fiber.Config{
-		AppName:      config.GetString("app.name"),
-		ErrorHandler: NewErrorHandler(),
-		Prefork:      config.GetBool("web.prefork"),
-	})
+    var app = fiber.New(fiber.Config{
+        AppName:      config.GetString("app.name"),
+        ErrorHandler: NewErrorHandler(),
+        Prefork:      config.GetBool("web.prefork"),
+    })
 
-	return app
+    return app
 }
 
 func NewErrorHandler() fiber.ErrorHandler {
-	return func(ctx *fiber.Ctx, err error) error {
-		code := fiber.StatusInternalServerError
-		if e, ok := err.(*fiber.Error); ok {
-			code = e.Code
-		}
+    return func(ctx *fiber.Ctx, err error) error {
+        code := fiber.StatusInternalServerError
+        message := err.Error()
 
-		return ctx.Status(code).JSON(fiber.Map{
-			"errors": err.Error(),
-		})
-	}
+        switch e := err.(type) {
+        case *model.ApiError:
+            code = e.StatusCode
+            message = e.Message
+        case *fiber.Error:
+            code = e.Code
+            message = e.Message
+        }
+        return ctx.Status(code).JSON(fiber.Map{
+            "errors": message,
+        })
+    }
 }
