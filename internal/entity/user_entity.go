8ca48d331ba06296d67c81d18d41b98af4e92702
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 19:59:55 +0700

    refactor user entity to use email as primary key and update foreign key references in tasks

--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -3,15 +3,14 @@ package entity
 import "time"
 
 type User struct {
-	ID          int       `gorm:"column:id;primaryKey;autoIncrement"`
+	Email       string    `gorm:"column:email;PrimaryKey;uniqueIndex"`
 	Name        string    `gorm:"column:name;"`
-	Email       string    `gorm:"column:email;"`
 	Password    string    `gorm:"column:password;"`
 	Token       string    `gorm:"column:token;"`
 	AccessToken string    `gorm:"-"`
 	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;"`
 	UpdatedAt   time.Time     `gorm:"column:updated_at;autoCreateTime;autoUpdateTime;"`
-	Task        []Task    `gorm:"foreignKey:user_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Task        []Task    `gorm:"foreignKey:email;references:email;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 func (User) TableName() string {
