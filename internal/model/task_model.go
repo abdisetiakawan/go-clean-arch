916d5ed48ec562e65e0b9f009f647b79cfd5b3d4
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-02 22:11:45 +0700

    add GetTaskRequest model for task retrieval with ID and Email validation

--- internal/model/task_model.go
+++ internal/model/task_model.go
@@ -28,3 +28,8 @@ type SearchTaskRequest struct {
 	Page   int    `json:"page" validate:"min=1"`
 	Size   int    `json:"size" validate:"min=1,max=100"`
 }
+
+type GetTaskRequest struct {
+	ID 	  string   	`json:"-" validate:"required"`
+	Email string 	`json:"-" validate:"required"`
+}
