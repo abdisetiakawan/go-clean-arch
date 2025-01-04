c140cb58082852f603bac72a8351f42886dc7d2a
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-04 08:05:30 +0700

    implement tag creation, listing, retrieval, updating, and deletion with user authentication

--- internal/delivery/http/tag_controller.go
+++ internal/delivery/http/tag_controller.go
@@ -1,11 +1,13 @@
 package http
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/sirupsen/logrus"
+	"math"
 
+	"github.com/abdisetiakawan/go-clean-arch/internal/delivery/http/middleware"
 	"github.com/abdisetiakawan/go-clean-arch/internal/model"
 	"github.com/abdisetiakawan/go-clean-arch/internal/usecase"
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
 )
 
 type TagsController struct {
@@ -20,63 +22,89 @@ func NewTagsController(useCase *usecase.TagUseCase, logger *logrus.Logger) *Tags
 	}
 }
 
-func (c *TagsController) List(ctx *fiber.Ctx) error {
-	responses, err := c.UseCase.List(ctx.UserContext())
-	if err != nil {
-		c.Log.Warnf("Failed to list tags : %+v", err)
-		return err
-	}
-	return ctx.JSON(model.WebResponse[[]model.TagResponse]{Data: responses})
-}
-
 func (c *TagsController) Create(ctx *fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
 	request := new(model.CreateTagRequest)
 	if err := ctx.BodyParser(request); err != nil {
-		c.Log.Warnf("Failed to parse request body : %+v", err)
-		return fiber.ErrBadRequest
+		c.Log.Warnf("failed to parse request body: %+v", err)
+		return model.ErrBadRequest
 	}
+	request.Email = auth.Email
 	response, err := c.UseCase.Create(ctx.UserContext(), request)
 	if err != nil {
-		c.Log.Warnf("Failed to create tag : %+v", err)
+		c.Log.Warnf("failed to create tag: %+v", err)
+	}
+	return ctx.JSON(model.WebResponse[*model.TagResponse]{Data: response})
+}
+
+func (c *TagsController) List(ctx *fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
+	request := &model.SearchTagRequest{
+		Email: auth.Email,
+		Name: ctx.Query("name", ""),
+		Page: ctx.QueryInt("page", 1),
+		Size: ctx.QueryInt("size", 10),
+	}
+
+	responses, total, err := c.UseCase.Search(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.Warnf("failed to list tag: %+v", err)
+		return err
+	}
+	paging := &model.PageMetadata{
+		Page: request.Page,
+		Size: request.Size,
+		TotalItem: total,
+		TotalPage: int64(math.Ceil(float64(total) / float64(request.Size))),
+	}
+
+	return ctx.JSON(model.WebResponse[[]model.TagResponse]{Paging: paging, Data: responses})
+}
+
+func (c *TagsController) Get(ctx *fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
+	request := &model.GetTagRequest{
+		ID: ctx.Params("tagId"),
+		Email: auth.Email,
+	}
+
+	response, err := c.UseCase.Get(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.Warnf("Failed to get tag : %+v", err)
+		return err
 	}
 	return ctx.JSON(model.WebResponse[*model.TagResponse]{Data: response})
 }
 
 func (c *TagsController) Update(ctx *fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
 	request := new(model.UpdateTagRequest)
 	if err := ctx.BodyParser(request); err != nil {
 		c.Log.Warnf("Failed to parse request body : %+v", err)
 		return fiber.ErrBadRequest
 	}
+	request.Email = auth.Email
+	request.ID = ctx.Params("tagId")
 	response, err := c.UseCase.Update(ctx.UserContext(), request)
 	if err != nil {
 		c.Log.Warnf("Failed to update tag : %+v", err)
+		return err
 	}
 	return ctx.JSON(model.WebResponse[*model.TagResponse]{Data: response})
 }
+
 
 func (c *TagsController) Delete(ctx *fiber.Ctx) error {
-	request := new(model.DeleteTagRequest)
-	if err := ctx.BodyParser(request); err != nil {
-		c.Log.Warnf("Failed to parse request body : %+v", err)
-		return fiber.ErrBadRequest
+	auth := middleware.GetUser(ctx)
+	request := &model.GetTagRequest{
+		ID: ctx.Params("tagId"),
+		Email: auth.Email,
 	}
-	response, err := c.UseCase.Delete(ctx.UserContext(), request)
-	if err != nil {
-		c.Log.Warnf("Failed to delete tag : %+v", err)
-	}
-	return ctx.JSON(model.WebResponse[*model.TagResponse]{Data: response})
-}
 
-func (c *TagsController) Get(ctx *fiber.Ctx) error {
-	request := new(model.GetTagRequest)
-	if err := ctx.BodyParser(request); err != nil {
-		c.Log.Warnf("Failed to parse request body : %+v", err)
-		return fiber.ErrBadRequest
+	if err := c.UseCase.Delete(ctx.UserContext(), request); err != nil {
+		c.Log.Warnf("Failed to delete tag : %+v", err)
+		return err
 	}
-	response, err := c.UseCase.Get(ctx.UserContext(), request)
-	if err != nil {
-		c.Log.Warnf("Failed to get tag : %+v", err)
-	}
-	return ctx.JSON(model.WebResponse[*model.TagResponse]{Data: response})
+	
+	return ctx.JSON(model.WebResponse[bool]{Data: true})
 }
