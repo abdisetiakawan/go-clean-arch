23f535a50e34c7f86ec68030c130b2e7e45a2280
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-04 08:08:18 +0700

    fix(TaskTag): correct foreign key references in TaskTag entity

--- internal/entity/task_tag_entity.go
+++ internal/entity/task_tag_entity.go
@@ -1,11 +1,11 @@
 package entity
 
 type TaskTag struct {
-    ID     uint `gorm:"column:id;primaryKey;autoIncrement"`
-    TaskId uint `gorm:"column:task_id;not null;index"`
-    TagId  uint `gorm:"column:tag_id;not null;index"`
-    Task   Task `gorm:"foreignKey:TaskId;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-    Tag    Tag  `gorm:"foreignKey:TagId;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	ID     uint `gorm:"column:id;primaryKey;autoIncrement"`
+	TaskId uint `gorm:"column:task_id;not null;index"`
+	TagId  uint `gorm:"column:tag_id;not null;index"`
+	Task   Task `gorm:"foreignKey:task_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Tag    Tag  `gorm:"foreignKey:tag_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 func (TaskTag) TableName() string {
