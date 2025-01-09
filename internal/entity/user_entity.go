ea5b015b5072ced04a4f25b64709f2dbb877ea5f
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-09 12:44:09 +0700

    refactor: remove unused Token field from User entity

--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -6,7 +6,6 @@ type User struct {
     Email       string    `gorm:"column:email;primaryKey;type:varchar(150);uniqueIndex"`
     Name        string    `gorm:"column:name;type:varchar(100);not null"`
     Password    string    `gorm:"column:password;type:varchar(255);not null"`
-    Token       string    `gorm:"column:token;type:varchar(255)"`
     AccessToken string    `gorm:"-"`
     CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
     UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
